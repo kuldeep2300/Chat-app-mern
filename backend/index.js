9bb40ac70207995a10587becc881fbcc2c379f96
Kuldeep Verma <kv5228920@gmail.com>
2024-10-27 20:28:54 +0530

    Project Completed

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const connectToMongoDB = require("./db/connectToMongoDB.js");
 const cors = require("cors");
+const path = require("path");
 
 // Routes
 const authRoute = require("./routes/auth");
@@ -11,17 +12,20 @@ const messageRoute = require("./routes/message");
 const userRoute = require("./routes/user");
 
 const protectRoute = require("./middlewares/protectRoute.js");
-const { app, server } = require('./socket/socket.js');
+const { app, server } = require("./socket/socket.js");
 
 const PORT = process.env.PORT || 5000;
+// const __dirname = path.resolve(); // Make sure this line is declared correctly
 
 app.use(express.json()); // To parse the incoming requests with JSON paylaods (from req. body)
 app.use(cookieParser());
 
-app.use(cors({
-  origin: 'http://localhost:5173',    // Replace with your frontend URL
-  credentials: true,                  // Enable sending cookies in cross-origin requests, / Allows cookies to be sent and received
-}));                                  // To allow cross-origin requests
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend URL
+    credentials: true, // Enable sending cookies in cross-origin requests, / Allows cookies to be sent and received
+  })
+); // To allow cross-origin requests
 
 app.use(protectRoute("jwt"));
 app.use(express.urlencoded({ extended: true })); // To parse the incoming requests with urlencoded payloads (from req. body))
@@ -29,6 +33,13 @@ app.use(express.urlencoded({ extended: true })); // To parse the incoming reques
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
+
+// Middleware - // Serve static files from the correct path
+app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB(process.env.MONGO_DB_URL); // To connect to MongoDB database
