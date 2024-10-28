5f98dd9478e2dcca9f014970b209a223c7c9fe2f
Kuldeep Verma <kv5228920@gmail.com>
2024-10-28 19:57:43 +0530

    Commit

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,13 +3,10 @@ import { Sidebar } from "../../components/sidebar/Sidebar";
 
 export const Home = () => {
   return (
-    // <div className="w-full flex max-h-screen bg-white ">
-    //   <Sidebar />
-    //   <MessageContainer />
-    // </div>
-    <div className="w-full flex full-container bg-white ">
+    <div className="w-full flex h-screen bg-white ">
       <Sidebar />
       <MessageContainer />
     </div>
+    
   );
 };
