92730baf4514a510dc6028acf466074cee8ff22b
Kuldeep Verma <kv5228920@gmail.com>
2024-10-28 11:13:27 +0530

    Project Completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,6 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://chat-app-mern-dh70.onrender.com",
-        changeOrigin: true,
-        secure: true,
       },
     },
   }
