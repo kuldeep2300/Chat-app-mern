1b21d0242b4a51b5ba45c09e7dbf9d3fee96cf51
Kuldeep Verma <kv5228920@gmail.com>
2024-10-28 21:11:09 +0530

    Making it responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import { Sidebar } from "../../components/sidebar/Sidebar";
 
 export const Home = () => {
   return (
-    <div className="w-[100vw] flex h-screen bg-white ">
+    <div className="w-[100vw] flex h-[100vh] bg-white ">
       <Sidebar />
       <MessageContainer />
     </div>
